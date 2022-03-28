ca677f3cc75d234b519ebb1ea61dc5e9a64ea0e5
imevanc <charal.vag@gmail.com>
2022-03-28 14:33:21 +0100

    created the usercards

--- src/components/MapLayout.js
+++ src/components/MapLayout.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import LinearColor from "./LinearColor";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-
+import Paper from "@mui/material/Paper";
+import coordinates from "../coord.json";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import UserCard from "./UserCard";
 
 const configLeaflet = () => {
   delete L.Icon.Default.prototype._getIconUrl;
@@ -16,56 +17,53 @@ const configLeaflet = () => {
     shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
   });
 };
-
 const MapLayout = () => {
   configLeaflet();
-  const [coordinates, setCoordinates] = useState({ lon: "", lat: "" });
-  const pcode = "L3+3AF";
-  const url = `http://api.getthedata.com/postcode/${pcode}`;
-
-  useEffect(() => {
-    const fetchCoordinates = async () => {
-      await axios.get(url).then((response) => {
-        setCoordinates({
-          lon: Number(response.data.data.longitude),
-          lat: Number(response.data.data.latitude),
-        });
-      });
-    };
-    fetchCoordinates().catch((error) => console.log(error));
-  }, [url]);
-
   return (
-    <Grid container>
+    <Grid container sx={{ paddingTop: "50px" }}>
+      <Grid item xs={12} sm={8} md={5} elevation={6}>
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+      </Grid>
       <Grid item sm={6}>
         <Box direction="row" justifyContent="end" display="flex">
           {!(
-            coordinates.lon.toString().length &&
-            coordinates.lat.toString().length
+            coordinates.coordinates.length && coordinates.coordinates.length
           ) ? (
             <LinearColor />
           ) : (
             <MapContainer
               tap={Boolean(false)}
-              center={[coordinates.lat, coordinates.lon]}
-              zoom={20}
+              center={[
+                coordinates.coordinates[0].latitude,
+                coordinates.coordinates[0].longitude,
+              ]}
+              zoom={10}
             >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={[coordinates.lat, coordinates.lon]}>
-                <Popup>
-                  Post Code: {pcode}
-                  <br />
-                  Latitude: {coordinates.lat} <br /> Longitude:{" "}
-                  {coordinates.lon}
-                </Popup>
-              </Marker>
+              {coordinates.coordinates.map((coordinate, idx) => {
+                return (
+                  <Marker
+                    key={idx}
+                    position={[coordinate.latitude, coordinate.longitude]}
+                  >
+                    <Popup>
+                      Post Code: {"postcode"}
+                      <br />
+                      Latitude: {coordinate.latitude} <br /> Longitude:{" "}
+                      {coordinate.longitude}
+                    </Popup>
+                  </Marker>
+                );
+              })}
             </MapContainer>
           )}
-          {/* </Box>
-      </Container> */}
         </Box>
       </Grid>
     </Grid>
---
+++ src/components/UserCard.js
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+const UserCard = () => {
+  return (
+    <Box
+      sx={{
+        my: 2,
+        mx: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Card sx={{ display: "flex" }}>
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography component="div" variant="h6">
+              firstName lastName
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              postCode
+            </Typography>
+          </CardContent>
+          <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+            Traffic Light Icon
+          </Box>
+        </Box>
+        <CardMedia
+          component="img"
+          sx={{ width: "150px", height: "100px", paddingTop: "15px" }}
+          variant="rounded"
+          alt="Random Image"
+          src="https://source.unsplash.com/random"
+        />
+      </Card>
+    </Box>
+  );
+};
+
+export default UserCard;
